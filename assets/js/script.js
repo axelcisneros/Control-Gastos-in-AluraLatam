40a650bd7695b1e1d3ab10f4d09f6debcd8b236b
Axel Cisneros <axel.cisneros25@hotmail.com>
2024-09-19 12:29:33 -0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 let listaNombresGastos = [];
 let listaValoresGastos = [];
+let listaDescripcionGastos = [];
 
 function clickBoton() {
     let nombreGasto = document.getElementById('nombreGasto').value;
     let valorGasto = document.getElementById('valorGasto').value;
-    if(valorGasto >= '150'){
+    let descripcionGastos = document.getElementById('descripcion').value;
+    if(valorGasto >= 150){
         alert("Realizaste un gasto elevado revisalo");
 
     }
     listaNombresGastos.push(nombreGasto);
     listaValoresGastos.push(valorGasto);
+    listaDescripcionGastos.push(descripcionGastos);
     actualizarListaGastos();
 }
 
@@ -20,7 +23,8 @@ function actualizarListaGastos() {
     let totalGastos = 0;
     listaNombresGastos.forEach((elemento,posicion)=>{
         const valorGasto = Number(listaValoresGastos[posicion]);
-        htmlLista += `<li>${elemento} - USD ${valorGasto.toFixed(2)}
+        const descripcionGastos = listaDescripcionGastos[posicion];
+        htmlLista += `<li>${elemento} - USD ${valorGasto.toFixed(2)} - ${descripcionGastos}
                         <button id="botonFormulario" onclick="eliminarGasto(${posicion});">Eliminar</button>
                         <button id="botonFormulario" onclick="modificarGasto(${posicion});">Modificar</button>
                     </li>`;
@@ -36,6 +40,7 @@ function actualizarListaGastos() {
 function limpiar() {
     document.getElementById('nombreGasto').value = '';
     document.getElementById('valorGasto').value = '';
+    document.getElementById('descripcion').value = '';
 }
 
 function eliminarGasto(posicion){

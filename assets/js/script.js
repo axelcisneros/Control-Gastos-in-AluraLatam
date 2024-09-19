983edda21fa56074ee5470c923aa6f6734243e56
Axel Cisneros <axel.cisneros25@hotmail.com>
2024-09-19 17:05:42 -0600

    code modified

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,40 @@
 let listaNombresGastos = [];
 let listaValoresGastos = [];
 let listaDescripcionGastos = [];
+let gastoActual = -1;
 
 function clickBoton() {
     let nombreGasto = document.getElementById('nombreGasto').value;
     let valorGasto = document.getElementById('valorGasto').value;
     let descripcionGastos = document.getElementById('descripcion').value;
-    if(valorGasto >= 150){
-        alert("Realizaste un gasto elevado revisalo");
+    if (nombreGasto === '' || descripcionGastos === '' ||isNaN(valorGasto)) {
+        alert('Por favor, ingrese nombre del gasto, descripción y valor');
+        return;
+    }
+
+ 
+     if(valorGasto >= 150){
+        confirm("Cuidado! Has ingresado un gasto mayor a USD 150!");
 
     }
-    listaNombresGastos.push(nombreGasto);
-    listaValoresGastos.push(valorGasto);
-    listaDescripcionGastos.push(descripcionGastos);
+
+    if (gastoActual === -1) {
+
+        listaNombresGastos.push(nombreGasto);
+        listaValoresGastos.push(valorGasto);
+        listaDescripcionGastos.push(descripcionGastos);
+
+    } else {
+
+        listaNombresGastos[gastoActual] = nombreGasto;
+        listaValoresGastos[gastoActual] = valorGasto;
+        listaDescripcionGastos[gastoActual] = descripcionGastos;
+        gastoActual = -1; 
+        document.getElementById('botonActualizar').style.display = 'none';
+        document.getElementById('botonFormulario').style.display = 'block';
+
+    }
+
     actualizarListaGastos();
 }
 
@@ -25,8 +47,8 @@ function actualizarListaGastos() {
         const valorGasto = Number(listaValoresGastos[posicion]);
         const descripcionGastos = listaDescripcionGastos[posicion];
         htmlLista += `<li>${elemento} - USD ${valorGasto.toFixed(2)} - ${descripcionGastos}
-                        <button id="botonFormulario" onclick="eliminarGasto(${posicion});">Eliminar</button>
-                        <button id="botonFormulario" onclick="modificarGasto(${posicion});">Modificar</button>
+                        <button onclick="eliminarGasto(${posicion});">Eliminar</button>
+                        <button onclick="modificarGasto(${posicion});">Modificar</button>
                     </li>`;
         //Calculamos el total de gastos
         totalGastos += Number(valorGasto);
@@ -44,11 +66,35 @@ function limpiar() {
 }
 
 function eliminarGasto(posicion){
+    if(confirm('¿Seguro que quieres eliminar el gasto?')){
+
+    }else{
+        return;
+    }
     listaNombresGastos.splice(posicion,1);
     listaValoresGastos.splice(posicion,1);
+    listaDescripcionGastos.splice(posicion,1);
     actualizarListaGastos();
 }
 
-function modificarGasto(){
-    alert("Quieres modificar los datos?");
+function modificarGasto(posicion) {
+    if(confirm('¿Quieres modificar la información?')){
+
+    }else{
+        return;
+    }   
+    
+    document.getElementById('nombreGasto').value = listaNombresGastos[posicion];
+    document.getElementById('valorGasto').value = listaValoresGastos[posicion];
+    document.getElementById('descripcion').value = listaDescripcionGastos[posicion];
+    gastoActual = posicion;
+    document.getElementById('botonActualizar').style.display = 'block';
+    document.getElementById('botonFormulario').style.display = 'none';
+
+}
+
+function actualizarDatos(){
+        
+    clickBoton();
+
 }
